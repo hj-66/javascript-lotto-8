86f14a054fa72bbc09de281c6962bd079c892218
hj-66 <a01088200534@gmail.com>
2025-11-01 04:07:56 +0900

    feat: 당첨번호/보너스번호를 입력받는 기능 생성

--- src/view/ConsoleView.js
+++ src/view/ConsoleView.js
@@ -1,10 +1,20 @@
 import { Console } from "@woowacourse/mission-utils";
 
 class ConsoleView {
-    async readLottoNumber() {
-        const input = await Console.readLineAsync('구입금액을 입력해 주세요.');
-        return input;
-    }
+  async readLottoNumbers() {
+    const input = await Console.readLineAsync("구입금액을 입력해 주세요.");
+    return input;
+  }
+
+  async readWinningNumbers() {
+    const input = await Console.readLineAsync("당첨 번호를 입력해 주세요.");
+    return input;
+  }
+
+  async readBonusNumber() {
+    const input = await Console.readLineAsync("보너스 번호를 입력해 주세요.");
+    return input;
+  }
 }
 
 export default ConsoleView;
